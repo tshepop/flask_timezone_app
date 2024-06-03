b01bd7a8505dda7243a4539e3acbc94d461001d5
tshepo <potelo.tshepo@gmail.com>
2024-06-03 19:07:39 +0200

    Completed app code.

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+import csv
+from flask import Flask, request, render_template
+import arrow
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Good Morning"
+    zones = []
+
+    # get data from csv file
+    with open("time_zone.csv") as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        for idx, row in enumerate(header_row):
+            # print(idx, row)
+            pass
+
+        for row in reader:
+            zones.append(row[0])
+
+    # get form data
+    if request.method == "POST":
+        get_selected_zone = request.form.get("city_zone")
+
+    # Context - dictionary to populate the form select element
+    # and pass the data to the template
+    data = {
+        "zones": zones,
+        "name_zone": get_selected_zone,
+    }
+
+    # use the arrow package to get date and time
+    current_time = arrow.now(tz=get_selected_zone)
+
+    return render_template("index.html",
+                           current_time=current_time,
+                           data=data)
 
 
 if __name__ == "__main__":
